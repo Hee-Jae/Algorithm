91f0903475a732cf8b629aed1af1f59abbc75b45
heejae <jhj967878@naver.com>
2021-09-23 13:36:49 +0900

    9월 23일

---
+++ test/unsignedvsminus.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  size_t s = 0;
+  int m = -1;
+
+  if(s < m){
+    cout << "minus win!\n";
+  } else{
+    cout << "unsigned win!\n";
+  }
+}
